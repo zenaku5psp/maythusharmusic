8691d0444bbc260edf0af720020bc711d131aeca
maythushar-music <maythusharofficial@gmail.com>
2025-03-07 12:16:13 +0630

    Update play.py

--- maythusharmusic/utils/inline/play.py
+++ maythusharmusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "❥—————————"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "—❥————————"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "——❥———————"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "———❥——————"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "————❥—————"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "—————❥————"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "——————❥———"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "———————❥——"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "————————❥—"
+        bar = "————————◉—"
     else:
-        bar = "—————————❥"
+        bar = "—————————◉"
     buttons = [
          [
             InlineKeyboardButton(
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/BRANDEDKING8"
+                text=" ˹ ѕυᴘᴘσʀт ˼ ", url=f"https://t.me/sasukemusicbotsupportchat"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
+                text=" ˹ υᴘᴅᴧтєѕ ˼ ", url=f"https://t.me/sasukevipmusicbotsupport"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/BRANDEDKING8"
+                text=" ˹ ѕυᴘᴘσʀт ˼ ", url=f"https://t.me/sasukemusicbotsupportchat"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
+                text=" ˹ υᴘᴅᴧтєѕ ˼ ", url=f"https://t.me/sasukevipmusicbotsupport"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
