4d7e14df6de7ddefd5d25950f64504dd8dcc03a6
maythushar-music <maythusharofficial@gmail.com>
2025-03-07 12:04:01 +0630

    Update help.py

--- maythusharmusic/plugins/bot/help.py
+++ maythusharmusic/plugins/bot/help.py
@@ -41,8 +41,8 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_)
         
-        await update.reply_video(
-            video="https://graph.org/file/84d30d4fd04570c0e0256.mp4",
+        await update.reply_photo(
+            photo="https://files.catbox.moe/3ch25z.jpg",
             caption=_["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard)
 
 
@@ -50,8 +50,8 @@ async def helper_private(
 @LanguageStart
 async def help_com_group(client, message: Message, _):
     keyboard = private_help_panel(_)
-    await message.reply_video(
-        video="https://te.legra.ph/file/51293513e6af319726fe7.mp4",
+    await message.reply_photo(
+        photo="https://files.catbox.moe/3ch25z.jpg",
         caption=_["help_2"], reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
