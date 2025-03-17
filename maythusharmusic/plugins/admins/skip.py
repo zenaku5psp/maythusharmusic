62f1c15246b156db731f7877dce4fdc7692a662b
maythushar-music <maythusharofficial@gmail.com>
2025-03-17 07:38:51 +0630

    Update skip.py

--- maythusharmusic/plugins/admins/skip.py
+++ maythusharmusic/plugins/admins/skip.py
@@ -7,7 +7,7 @@ from maythusharmusic.core.call import Hotty
 from maythusharmusic.misc import db
 from maythusharmusic.utils.database import get_loop
 from maythusharmusic.utils.decorators import AdminRightsCheck
-from maythusharmusic.utils.inline import close_markup, stream_markup, stream_markup2
+from maythusharmusic.utils.inline import close_markup, stream_markup
 from maythusharmusic.utils.stream.autoclear import auto_clean
 from maythusharmusic.utils.thumbnails import get_thumb
 from config import BANNED_USERS
@@ -114,7 +114,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Hotty.skip_stream(chat_id, link, video=status, image=image)
         except:
             return await message.reply_text(_["call_6"])
-        button = stream_markup2(_, chat_id)
+        button = stream_markup(_, chat_id)
         img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
@@ -147,7 +147,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Hotty.skip_stream(chat_id, file_path, video=status, image=image)
         except:
             return await mystic.edit_text(_["call_6"])
-        button = stream_markup(_, videoid, chat_id)
+        button = stream_markup(_, chat_id)
         img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
@@ -167,7 +167,7 @@ async def skip(cli, message: Message, _, chat_id):
             await Hotty.skip_stream(chat_id, videoid, video=status)
         except:
             return await message.reply_text(_["call_6"])
-        button = stream_markup2(_, chat_id)
+        button = stream_markup(_, chat_id)
         run = await message.reply_photo(
             photo=config.STREAM_IMG_URL,
             caption=_["stream_2"].format(user),
@@ -190,7 +190,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await message.reply_text(_["call_6"])
         if videoid == "telegram":
-            button = stream_markup2(_, chat_id)
+            button = stream_markup(_, chat_id)
             run = await message.reply_photo(
                 photo=config.TELEGRAM_AUDIO_URL
                 if str(streamtype) == "audio"
@@ -216,7 +216,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
         else:
-            button = stream_markup(_, videoid, chat_id)
+            button = stream_markup(_, chat_id)
             img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
