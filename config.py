72430cde2197cf2b14310916df108c2655a1c2d6
maythushar-music <maythusharofficial@gmail.com>
2025-05-01 20:17:21 +0630

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,11 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
+SUPPORT_CHANNEL_LINK = getenv("SUPPORT_CHANNEL_LINK", "@sasukevipmusicbotsupport")
+SUPPORT_CHAT_LINK = getenv("SUPPORT_CHAT_LINK", "@sasukemusicsupportchat")
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sasukevipmusicbotsupport")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+h-lrv0RTtcs4NGQ1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sasukemusicsupportchat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
