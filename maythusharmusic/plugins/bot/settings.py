727314b18bceb3e9142ec56316e962ff96a7c65c
maythushar-music <maythusharofficial@gmail.com>
2025-05-01 20:24:10 +0630

    Update settings.py

--- maythusharmusic/plugins/bot/settings.py
+++ maythusharmusic/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://files.catbox.moe/7ygwo6.mp4"),
+        InputMediaVideo("https://files.catbox.moe/jy8o13.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
