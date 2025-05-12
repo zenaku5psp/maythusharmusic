c686f8dd2a491c90a95f9c24a46b6b1de552a805
wanglintst5un <pyaesoneofficial15@gmail.com>
2025-05-12 22:38:57 +0630

    Better

--- maythusharmusic/__init__.py
+++ maythusharmusic/__init__.py
@@ -25,6 +25,6 @@ SoundCloud = SoundAPI()
 Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
-YouTube = YouTube()
+YouTube = YouTubeAPI()
 
 APP = "sasukevipmusicbot"  # connect music api key "Dont change it"
