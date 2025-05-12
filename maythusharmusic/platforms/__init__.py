9e78eb5c3dbf6b5507411ab39b4b6d1f795f11e3
wanglintst5un <pyaesoneofficial15@gmail.com>
2025-05-12 22:38:21 +0630

    Better

--- maythusharmusic/platforms/__init__.py
+++ maythusharmusic/platforms/__init__.py
@@ -4,15 +4,4 @@ from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
-from .Youtube import YouTube
-
-
-class PlaTForms:
-    def __init__(self):
-        self.apple = Apple()
-        self.carbon = Carbon()
-        self.resso = Resso()
-        self.soundcloud = SoundCloud()
-        self.spotify = Spotify()
-        self.telegram = Telegram()
-        self.youtube = YouTube()
+from .Youtube import YouTubeAPI
