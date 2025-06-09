5ce4a87b86c63e15970d3f2c2f78c976d197fb46
zenaku565usm <pyaesone55usm@gmail.com>
2025-06-09 19:51:33 +0630

    Update Youtube.py

--- maythusharmusic/platforms/Youtube.py
+++ maythusharmusic/platforms/Youtube.py
@@ -20,8 +20,8 @@ import time
 
 
 # ✅ Configurable constants
-API_KEY = "Rf1qda5gyCITj6VbrekzRxmR"
-API_BASE_URL = "http://deadlinetech.site"
+API_KEY = "rSXz30zCJkQnByYyQOAEUw"
+API_BASE_URL = "https://my.sanatanixtech.site"
 
 MIN_FILE_SIZE = 51200
 
