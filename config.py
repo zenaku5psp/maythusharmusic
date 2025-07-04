399c06d81df20c6a229fbbde6aea4d7f250134cd
zenaku5psp <zenaku5psp@outlook.com>
2025-07-04 07:47:34 +0630

    Update config.py

--- config.py
+++ config.py
@@ -16,12 +16,14 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 
 STICKER_ID = "CAACAgUAAxkBAAEObxloHODEbJMRLG0DgnPYJ7bOUXc5QwACmRkAAg6V6FSBJlu8dUgdCTYE"
 
+COOKIE_URL = getenv("COOKIE_URL")
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
